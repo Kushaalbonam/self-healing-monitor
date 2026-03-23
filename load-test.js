7b977f6d44051ea88fa193b657e5e522569f6ce3
Kushaal Bonam <kushaalbonam0529@gmail.com>
2026-03-23 05:56:20 +0000

    Add CI/CD performance gate with k6

---
+++ load-test.js
@@ -0,0 +1,20 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export const options = {
+  vus: 10,           // 10 Virtual Users
+  duration: '10s',   // Run for 10 seconds
+  thresholds: {
+    http_req_duration: ['p(95)<250'], // 95% of requests must be faster than 250ms
+  },
+};
+
+export default function () {
+  const res = http.get('http://localhost:8080/');
+  
+  check(res, {
+    'status is 200': (r) => r.status === 200,
+  });
+
+  sleep(1);
+}
